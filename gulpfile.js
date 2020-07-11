7b2f8bcfae0d71f9ee8b5e3e3c1f85f0009c10fd
tuan nguyen <tuanbeovnn@gmail.com>
2020-07-12 00:16:52 +0300

    First Version

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  	return gulp.src('./scss/style.scss')
+    	.pipe(sass())
+    	.pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('scss/*.scss', ['sass']);
+});
+

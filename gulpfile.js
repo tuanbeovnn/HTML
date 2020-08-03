745e70ea4ae61b681c33c91e7fa06793bcdb4313
tuanbeovnn <tuanquangnguyen1710@gmail.com>
2020-08-03 22:10:28 +0300

    Responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+
+const gulp = require("gulp")
+const sass = require("gulp-sass")
+
 
 gulp.task('sass', function(){
-  	return gulp.src('./scss/style.scss')
-    	.pipe(sass())
-    	.pipe(gulp.dest('./'))
+  	return gulp.src('./sass/style.scss')
+	  .pipe(sass().on('error', sass.logError))
+	  .pipe(gulp.dest('./css'))
 });
 
-gulp.task('watch', function() {
-	gulp.watch('scss/*.scss', ['sass']);
-});
+gulp.task('watch', function(){
+	gulp.watch('app/scss/**/*.scss', ['sass']); 
+	// Other watchers
+})
 
+
+
+
+
+ 
